ff0b4a77970326ecd51d0285af166c27a3ed097d
Corey Ford <corey@coreyford.name>
2014-09-14 20:47:37 -0700

    rearrange templating

--- mail2git.rb
+++ mail2git.rb
@@ -10,19 +10,15 @@ mail = Mail.read_from_string(STDIN.read)
 from_addr = mail.from[0]
 from_name = mail['from'].display_names[0] if mail['from']
 from_name = from_addr if from_name.nil? or from_name.strip.empty?
+slug = mail.subject.downcase.gsub(/\W+/, ' ').strip.tr(' ', '-')
 params = {
-  'subject' => mail.subject,
-  'slug' => mail.subject.downcase.gsub(/\W+/, ' ').strip.tr(' ', '-'),
-  'date' => mail.date,
-  'body' => mail.body.to_s,
-  'from' => from_name
+  'subject' => mail.subject, 'slug' => slug, 'date' => mail.date,
+  'from' => from_name, 'body' => mail.body.to_s
 }
-path_template    = Liquid::Template.parse(config['file']['path'])
-content_template = Liquid::Template.parse(config['file']['content'])
-message_template = Liquid::Template.parse(config['git']['message'])
-path    = path_template.render(params)
-content = content_template.render(params)
-message = message_template.render(params)
+
+path    = Liquid::Template.parse(config['file']['path']).render(params)
+content = Liquid::Template.parse(config['file']['content']).render(params)
+message = Liquid::Template.parse(config['git']['message']).render(params)
 
 repo = Rugged::Repository.new(config['git']['repo'])
 oid = repo.write(content, :blob)
