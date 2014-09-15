5399195213a48aa7094f6c1c598e8fc9ec175a14
Corey Ford <corey@coreyford.name>
2014-09-14 17:44:21 -0700

    Liquid templating

--- mail2git.rb
+++ mail2git.rb
@@ -7,23 +7,37 @@ config = YAML.load_file('config.yml')
 
 mail = Mail.read_from_string(STDIN.read)
 
+params = {
+  'subject' => mail.subject,
+  'slug' => mail.subject.downcase.gsub(/\W+/, ' ').strip.tr(' ', '-'),
+  'date' => mail.date,
+  'body' => mail.body.to_s,
+  'from' => mail.from
+}
+path_template    = Liquid::Template.parse(config['file']['path'])
+content_template = Liquid::Template.parse(config['file']['content'])
+message_template = Liquid::Template.parse(config['git']['message'])
+path    = path_template.render(params)
+content = content_template.render(params)
+message = message_template.render(params)
+
 repo = Rugged::Repository.new(config['git']['repo'])
-oid = repo.write(mail.body.to_s, :blob)
+oid = repo.write(content, :blob)
 index = repo.index
 index.read_tree(repo.head.target.tree)
-index.add(path: 'test.txt', oid: oid, mode: 0100644)
+index.add(path: path, oid: oid, mode: 0100644)
 
 Rugged::Commit.create(repo, {
   tree: index.write_tree(repo),
-  message: mail.subject,
+  message: message,
   author: {
     email: mail.from[0],
     name: mail.from[0],
     time: mail.date.to_time
   },
   committer: {
-    email: config['git']['commit']['email'],
-    name: config['git']['commit']['name'],
+    email: config['git']['email'],
+    name: config['git']['name'],
     time: Time.now
   },
   parents: [ repo.head.target ].compact,
